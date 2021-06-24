60792bdf46baee4bf408d2129c0e192487fe6f37
pbruynel <paulbruynel@gmail.com>
2021-06-25 00:27:26 +0200

    Cleanup, removed commented code.

--- server.R
+++ server.R
@@ -5,36 +5,22 @@ source('setYLabel.R')
 source('setTitle.R')
 source('setSubtitle.R')
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw the plot
 server <- function(input, output) {
     df <- prepareData()
 
     output$distPlot <- renderPlot({
         # load data if necessary
         dataIsActual <- (Sys.Date() - ddays(2)) < max(df$Date_of_report)
-        # print(paste("max date =", max(df$Date_of_report)))
         # if user wants latest data and data is not actual, then load latest data
         # else if user wants local data and data is actual then load local data
         # else no action required
         if (input$actualData & !dataIsActual){
-            # print("load actual data")
             df <<- prepareData(update=TRUE)
             dataIsActual <- (Sys.Date() - ddays(2)) < max(df$Date_of_report)
-            # print(paste0("Update = ", input$actualData, 
-            #              "; data is actual = ", dataIsActual, 
-            #              "; max date =", max(df$Date_of_report)))
         } else if (!input$actualData & dataIsActual) {
-            # print("load local data")
             df <<- prepareData(update=FALSE)
             dataIsActual <- (Sys.Date() - ddays(2)) < max(df$Date_of_report)
-            # print(paste0("Update = ", input$actualData, 
-            #              "; data is actual = ", dataIsActual, 
-            #              "; max date =", max(df$Date_of_report)))
-        } else {
-            # print("no action required")
-            # print(paste0("Update = ", input$actualData, 
-            #              "; data is actual = ", dataIsActual, 
-            #              "; max date =", max(df$Date_of_report)))
         }
 
         df <- df %>%
@@ -47,15 +33,6 @@ server <- function(input, output) {
         plotSubtitle <- setSubtitle(yAxis, input$dateRange[1], input$dateRange[2], df)
         plotCaption <- "Source: RIVM Netherlands (https://data.rivm.nl/covid-19/)"
 
-        # df %>%
-        #     ggplot(aes(x=Date_of_report, y=input$y, group=Province, color=Province)) +
-        #     geom_line()
-
-        # df %>%
-        #     ggplot(aes_string(x = df$Date_of_report, y = input$y)) +
-        #     geom_line() +
-        #     labs(x = "Date", y = yLabel, title = plotTitle)
-        
         df %>%
             ggplot(aes_string(x = df$Date_of_report, y = input$y)) +
             geom_line() +
@@ -63,30 +40,8 @@ server <- function(input, output) {
             theme(
                 plot.title = element_text(hjust = 0.5, size = 16),    # Center title position and size
                 plot.subtitle = element_text(hjust = 0.5, size = 14),            # Center subtitle
-                plot.caption = element_text(hjust = 0, face = "italic", size = 12) # move caption to the left
+                plot.caption = element_text(hjust = 0, face = "italic", size = 12)  # move caption to the left
             )
-        
-        # if (yAxis==Total_reported) {
-        #     df %>% ggplot(aes(x=Date_of_report, y=Total_reported, group=Province, color=Province)) +
-        #         geom_line()
-        # } else if (yAxis==Hospital_admission) {
-        #     ggplot(data = df, aes(x=Date_of_report, y=Hospital_admission, group=Province, color=Province)) +
-        #         geom_line()
-        # } else {
-        #     ggplot(data = df, aes(x=Date_of_report, y=Total_deceased, group=Province, color=Province)) +
-        #         geom_line()
-        # }
     })
-
-    output$summary <- renderText({
-        input$actualData
-        setSummary(input$y, input$dateRange[1], input$dateRange[2], df)
-    })
-    
-    output$update <- renderText({
-        paste0("Update = ", input$actualData, 
-               "; max date =", max(df$Date_of_report))
-    })
-    
 
 }
--- ui.R
+++ ui.R
@@ -1,20 +1,19 @@
 library(shiny)
 source('prepareData.R')
+
 df <- prepareData()
 minDate <- min(df$Date_of_report)
 maxDate <- Sys.Date()
 
-
-
-# Define UI for application that draws a histogram
+# Define UI for application that draws a plot
 ui <- fluidPage(
     # Application title
     titlePanel("Covid19 in the Netherlands"),
     
-    # Sidebar with a slider input for number of bins 
+    # Sidebar with input elements
     sidebarLayout(
         sidebarPanel(
-            HTML(paste0("Covid19 figures will be plotted. 
+            HTML(paste0("Covid19 figures will be plotted.<br/> 
                  Pick dates between ", minDate, " and ", maxDate, ".")),
             br(), br(),
             
@@ -29,25 +28,25 @@ ui <- fluidPage(
                                     "Deceased"="Total_deceased"),
                         selected = "Total_reported"),
 
-            checkboxInput("actualData", "Update to latest data", value = FALSE),
+            checkboxInput("actualData", "Load latest data", value = FALSE)
             
         ),
         
-        # Show a plot of the Covid19 figures
+        # Show tabs with instructions and a plot of the Covid19 figures.
         mainPanel(
             tabsetPanel(type = "tabs", 
                         tabPanel("Instructions", br(), 
                                  "Tab 'Covid19 figures' shows Covid19 related figures of the Netherlands over a period of time.",
-                                 br(),
+                                 br(), br(),
                                  "On the sidebar panel you can specify the period you're interested in as well as the type of figures.",
                                  br(), br(),
                                  "The source of the data is ",
                                  a(href = "https://data.rivm.nl/covid-19/", "RIVM Netherlands"), ".",
-                                 "The figures
-                                 are updated on a daily basis by the RIVM. This application is initially loading locally stored data however,
-                                 for performance reasons. These locally stored data contain Covid19 figures up to June 17, 2021.",
-                                 "You can load the latest figures by selecting the checkbox 'Update to latest data'. Note that 
-it can take some time (10 - 20 seconds) for the data to be loaded and the plot to be refreshed."
+                                 "The figures are updated on a daily basis by the RIVM. 
+                                 This application is initially loading locally stored data however,
+                                 for performance reasons. These locally stored data contain Covid19 figures up to June 17, 2021.
+                                 You can load the latest figures by selecting the checkbox 'Load latest data'.
+                                 Note that it can take some time (10 - 20 seconds) for the data to be loaded and the plot to be refreshed."
                                  ),
                         tabPanel("Covid19 figures", br(), plotOutput("distPlot")) 
             )
